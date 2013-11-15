259cca8ee45d2230a25eee531ab39e50b9c32279
Daniel Zabinskas <daniel.zabinskas@clemenger.com.au>
2013-11-15 20:11:22 +1100

    continued dev

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,7 +1,17 @@
 var cubes = [];
 $(document).ready(function() {
 
-	// Cube colors:
+	//Display config values
+	var cubegutter = 100;
+	var cubewidth = 200;
+	var hexValues = { //Colour display values
+		P: "#8d06ff",
+		Y: "#f0e242",
+		G: "#40ac4f",
+		B: "#2372d4"
+	};
+
+	// Configurtion of colours for the cube faces:
 	//  P 
 	// YGB
 	//  B 
@@ -55,39 +65,71 @@ $(document).ready(function() {
 			back: "Y"
 		}
 	];
-	var hexValues = {
-		P: "#8d06ff",
-		Y: "#f0e242",
-		G: "#40ac4f",
-		B: "#2372d4"
+
+	//The three possible sequences of faces that orbit the X axis in different cube orientation
+	var faceSequences = {
+		A: ["top", "front", "bottom", "back"],
+		B: ["top", "right", "bottom", "left"],
+		C: ["left", "front", "right", "back"]
 	};
-	var faceSequences = [
-		["top", "front", "bottom", "back"],
-		["top", "right", "bottom", "left"],
-		["front", "right", "back", "left"]
-	]
-	var cubegutter = 100;
-	var cubewidth = 200;
+
+	//Initialize each cube
 	$(".cube").each(function(index) {
 		var $this = $(this);
-		var def = {};
+		var def = {}; //Create object that will hold all data for a cube
 		var i;
 		def.$cube = $this;
+		//Get the color configuration and set the faces to display these
 		def.colours = cubeColours[index];
 		for (i in def.colours) {
 			// Set the colours of the cube faces with the colours from the cubeColours definition
 			def.$cube.find(".face." + i).css("background-color", hexValues[def.colours[i]]);
 		}
+		//Setup cube orientation values
 		def.tX = index * cubewidth + index * cubegutter;
 		def.rX = 0;
 		def.rY = 0;
 		def.rZ = 0;
-		transformCube(def);
+
+		//Put the cube definition into the cubes array
 		cubes.push(def);
+
+		//Set the orientation of the DOM to reflect the data
+		transformCube(def);
 	});
 
-	turnRow(180);
+	// turnRow(180);
 });
+
+function moveToSequence(def, sequence, direction, index) {
+	if (sequence == "A") {
+		if (direction == "F") {
+			def.rY = 0;
+			def.rZ = 0;
+		} else if (direction == "R") {
+			def.rY = 180;
+			def.rZ = 0;
+		}
+	} else if (sequence == "B") {
+		if (direction == "F") {
+			def.rY = -90;
+			def.rZ = 0;
+		} else if (direction == "R") {
+			def.rY = 90;
+			def.rZ = 90;
+		}
+	} else if (sequence == "C") {
+		if (direction == "F") {
+			def.rY = 0;
+			def.rZ = 90;
+		} else if (direction == "R") {
+			def.rY = 180;
+			def.rZ = 90;
+		}
+	}
+	def.rX = index * 90;
+	transformCube(def);
+}
 
 function rotateXto(def, deg) {
 	def.rX = deg;
